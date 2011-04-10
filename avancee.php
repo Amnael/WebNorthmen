f317936ad0fe54b501145107a7dbcebe0e2be512
Amnael <musashi50@hotmail.fr>
2011-04-10 14:54:54 +0200

    Ajout du Système d'avancée

--- avancee.php
+++ avancee.php
@@ -1,1 +1,4 @@
-Test
+<img src="IMAGE/raid/baradin.png" alt="baradin" /> <br />1/1<p>
+<img src="IMAGE/raid/blackwing.png" alt="descente" /> <br />5/6 - HM : 0/6<p>
+<img src="IMAGE/raid/throne.png" alt="throne" /> <br />1/2 - HM : 0/2<p>
+<img src="IMAGE/raid/bastion.png" alt="bastion" /> <br />4/4 - HM : 0/5<p>
