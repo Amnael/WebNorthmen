a13e095f254da78c54dee822d878c88928721e72
Amnael <musashi50@hotmail.fr>
2011-04-10 12:29:34 +0200

    Amélioration graphique du menu

--- menu.php
+++ menu.php
@@ -1,1 +1,3 @@
-Acceuil
+<div id="centrer_menu">
+ <li><a href='index.php'>Acceuil</a></li><li><a href='histoire.php'>Histoire</a></li><li><a href='clan.php'>Le Clan</a></li><li><a href='avancement.php'>Avancée PvE</a></li><li><a href='divers.php'>Divers</a></li>
+</div>
