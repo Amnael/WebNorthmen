2769cdc82f57786d536972be1a9172202dcf3da4
Amnael <musashi50@hotmail.fr>
2011-04-10 16:31:03 +0200

    Ajout des Terres Du Feu

--- avancee.php
+++ avancee.php
@@ -2,3 +2,4 @@
 <img src="IMAGE/raid/blackwing.png" alt="descente" /> <br />5/6 - HM : 0/6<p>
 <img src="IMAGE/raid/throne.png" alt="throne" /> <br />1/2 - HM : 0/2<p>
 <img src="IMAGE/raid/bastion.png" alt="bastion" /> <br />4/4 - HM : 0/5<p>
+<img src="IMAGE/raid/fireland.png" alt="fireland" /> <br />Prochainement<p>
